edd815215c90af815fb70e05715d8b3762bdc6c5
GuichenZhu <50548017+GET12306@users.noreply.github.com>
2026-06-23 16:50:29 +0900

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = {}  # results for already computed amounts
+
+        def dfs(amt):
+            if amt == 0:
+                return 0
+            
+            if amt in memo:
+                return memo[amt]
+
+            res = 1e9
+            for coin in coins:
+                if amt - coin >= 0:
+                    res = min(res, 1 + dfs(amt-coin))
+            memo[amt] = res
+            return res
+        
+        mincoin = dfs(amount)
+
+        if mincoin >= 1e9:
+            return -1
+        return mincoin
