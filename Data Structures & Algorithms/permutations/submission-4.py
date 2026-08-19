b75b61d91f7de80f4fe29f594c69c4e2fd5e4da4
GuichenZhu <50548017+GET12306@users.noreply.github.com>
2026-08-19 15:29:39 +0900

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+
+        def back(cur, used: list[bool]):
+            if sum(used) == n:
+                res.append(cur.copy())
+                return
+
+            for i, use in enumerate(used):
+                if not use:
+                    cur.append(nums[i])
+                    used[i] = True
+                    
+                    back(cur, used)
+                    
+                    cur.pop()
+                    used[i] = False
+                    
+
+
+        back([],[False] * n)
+        return res
